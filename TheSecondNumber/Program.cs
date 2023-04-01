ecc18914c7f8e55761819f92333c7b4bc428b220
besser <besser9009@mail.ru>
2023-04-02 02:37:50 +0300

    add TheSecondNumber

---
+++ TheSecondNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+System.Console.WriteLine("Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+System.Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int num2_1 = num % 100;
+int num2 = num2_1 / 10;
+System.Console.WriteLine($"Вторая цифра вашего числа: {num2}");
