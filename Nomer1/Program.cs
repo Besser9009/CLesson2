0500d0014ef01259653f87f7e3a36f3534f773d9
besser <besser9009@mail.ru>
2023-04-02 03:11:41 +0300

    add Nomer1

---
+++ Nomer1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+System.Console.WriteLine("Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+System.Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+System.Console.Write("Введите количество элементов в заданном вами числе: ");
+int n = int.Parse(Console.ReadLine());
+double z1 = Math.Pow(10, (n - 3));
+int z = Convert.ToInt32(z1);
+if (n == 3){
+    int num3 = num % 10;
+    System.Console.WriteLine(num3);
+}
+else if (n > 3){
+    int num3 = (num / z) % 10;
+    System.Console.WriteLine(num3);
+}
+else{
+    System.Console.WriteLine("Третьей цифры нет");
+}
